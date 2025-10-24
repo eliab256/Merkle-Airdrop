fb469cf9cfe36849c3923039d7e688202307f289
eliab256 <bordonielia96@gmail.com>
2025-10-24 14:32:44 +0000

    fixed tests

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -1,12 +1,12 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Test, Console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {Token} from "../src/Token.sol";
 
 contract MerkleAirdropTest is Test {
-    MerkleAirdrop public merkleAirdrop;
+    MerkleAirdrop public airdrop;
     Token public token;
 
     bytes32 public root = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
@@ -21,7 +21,7 @@ contract MerkleAirdropTest is Test {
 
     function setUp() public {
         token = new Token();
-        airdrop = new merkleAirdrop(root, token);
+        airdrop = new MerkleAirdrop(root, token);
         token.mint(token.owner(), initialSupply);
         token.transfer(address(airdrop), initialSupply);
         (user,userPrivatekey) = makeAddrAndKey("user");
@@ -31,9 +31,10 @@ contract MerkleAirdropTest is Test {
     function testUsersCanClaim() public {
         uint256 startingBalance = token.balanceOf(user);
         bytes32 digest = airdrop.getMessageHash(user, amountToClaim);
+        (uint8 v, bytes32 r, bytes32 s ) = vm.sign(userPrivatekey, digest); 
 
         vm.prank(user);
-        airdrop.claim(user, amountToClaim, proof);
+        airdrop.claim(user, amountToClaim, proof, v, r, s);
 
         assertEq(token.balanceOf(user) - startingBalance , amountToClaim);
     }
@@ -42,7 +43,7 @@ contract MerkleAirdropTest is Test {
         uint256 startingBalance = token.balanceOf(user);
         bytes32 digest = airdrop.getMessageHash(user, amountToClaim);
         //user sign a message
-        vm.prank(user);
+        //vm.prank(user);
         (uint8 v, bytes32 r, bytes32 s ) = vm.sign(userPrivatekey, digest); 
         //gasPayer calls claim using the signed message
         vm.prank(gasPayer);
